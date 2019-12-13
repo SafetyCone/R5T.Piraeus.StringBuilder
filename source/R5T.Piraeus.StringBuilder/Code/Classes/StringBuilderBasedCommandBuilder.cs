84aaaf053fd15f87fef8de8fbfc6eb40d9a3c3d3
MinexAutomation <minex.automation@gmail.com>
2019-12-12 22:49:42 -0800

    Implemented token separator logic.

--- source/R5T.Piraeus.StringBuilder/Code/Classes/StringBuilderBasedCommandBuilder.cs
+++ source/R5T.Piraeus.StringBuilder/Code/Classes/StringBuilderBasedCommandBuilder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using R5T.Magyar.Extensions;
+
 using TextStringBuilder = System.Text.StringBuilder;
 
 
@@ -8,16 +10,24 @@ namespace R5T.Piraeus.StringBuilder
     public class StringBuilderBasedCommandBuilder : ICommandBuilder
     {
         private TextStringBuilder StringBuilder { get; } = new TextStringBuilder();
+        private string TokenSeparator { get; }
         
+
+        public StringBuilderBasedCommandBuilder(string tokenSeparator = CommandTokenSeparator.DefaultValue)
+        {
+            this.TokenSeparator = tokenSeparator;
+        }
 
         public void Append(string token)
         {
+            var appendix = $"{token}{this.TokenSeparator}";
+
             this.StringBuilder.Append(token);
         }
 
         public string BuildCommand()
         {
-            var command = this.StringBuilder.ToString();
+            var command = this.StringBuilder.ToString().ExceptLast(this.TokenSeparator); // Build the string, then remove characters to allow multiple builds of the command.
             return command;
         }
     }
