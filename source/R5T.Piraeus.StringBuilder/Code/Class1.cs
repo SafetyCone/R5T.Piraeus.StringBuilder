356451099abe1f0dda4851550d4a742f25bbaccd
MinexAutomation <minex.automation@gmail.com>
2019-12-12 19:36:55 -0800

    Added StringBuilder-based implementation type for the ICommandBuilder interface.

--- source/R5T.Piraeus.StringBuilder/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Piraeus.StringBuilder
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Piraeus.StringBuilder/Code/Classes/StringBuilderBasedCommandBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using TextStringBuilder = System.Text.StringBuilder;
+
+
+namespace R5T.Piraeus.StringBuilder
+{
+    public class StringBuilderBasedCommandBuilder : ICommandBuilder
+    {
+        private TextStringBuilder StringBuilder { get; } = new TextStringBuilder();
+        
+
+        public void Append(string token)
+        {
+            this.StringBuilder.Append(token);
+        }
+
+        public string BuildCommand()
+        {
+            var command = this.StringBuilder.ToString();
+            return command;
+        }
+    }
+}
